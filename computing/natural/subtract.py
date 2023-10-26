9c204786ef655c444d6b17eb9d814b2a10cdf003
Fel1-of <fivehellday@gmail.com>
2023-10-27 00:17:39 +0300

    Fix some shit

--- computing/natural/subtract.py
+++ computing/natural/subtract.py
@@ -8,13 +8,12 @@ def subtract(self: Natural, other: Natural) -> Natural:
     num1 = self.copy()
     num2 = other.copy()
     if num1.compare(num2) == 1:
-        num1, num2 = num2, num1
+        raise ValueError
     for i in range(len(num2)):
         num1.data[i] = num1.data[i] - num2.data[i]
         if num1.data[i] < 0:
             num1.data[i] += 10
             num1.data[i+1] -= 1
-    while num1.data[-1] == 0:
+    while num1.data[-1] == 0 and len(num1) != 1:
         num1.data.pop()
     return num1
-
