9f3f14adcf466492f08bab4b1b92ae40e402da54
Fel1-of <fivehellday@gmail.com>
2023-10-27 00:06:11 +0300

    Fix some shit

--- computing/natural/subtract.py
+++ computing/natural/subtract.py
@@ -1,19 +1,20 @@
 # Модуль: SUB_NN_N
 # Автор: Комосский_Егор_2381
 
-from Natural import Natural
+from .Natural import Natural
 
 
-def subtract(num1: Natural, num2: Natural) -> Natural:
-    if num1 < num2:
+def subtract(self: Natural, other: Natural) -> Natural:
+    num1 = self.copy()
+    num2 = other.copy()
+    if num1.compare(num2) == 1:
         num1, num2 = num2, num1
     for i in range(len(num2)):
         num1.data[i] = num1.data[i] - num2.data[i]
         if num1.data[i] < 0:
             num1.data[i] += 10
             num1.data[i+1] -= 1
-    if num1.data[-1] == 0:
-        while num1.data[-1] == 0:
-            num1.data.pop()
+    while num1.data[-1] == 0:
+        num1.data.pop()
     return num1
 
