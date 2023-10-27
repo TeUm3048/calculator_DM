c890b0aa0990bb1b1b6a3b6d44efe499caaeac2b
SaveliyKrivov <krivovsaveliy28@gmail.com>
2023-10-27 20:52:49 +0300

    Increment added

--- computing/natural/increment.py
+++ computing/natural/increment.py
@@ -4,5 +4,13 @@
 from .Natural import Natural
 
 
-def increment(num: Natural) -> None:
-    pass
+def increment(self: Natural) -> Natural:
+    num = self.copy()
+    num.data[-1] + 1
+    if num.data[-1] > 9:
+        for i in range(len(num)-1, -1, -1):
+            if num.data[i] > 9:
+                num.data[i] = 0
+                num.data[i-1] += 1
+
+    return num
