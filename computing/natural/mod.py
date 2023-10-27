1c30b0803ff405c9765fc1a42c049117298d9091
enshxx <kuznecovilya12@gmail.com>
2023-10-27 23:47:53 +0300

    mod: done

--- computing/natural/mod.py
+++ computing/natural/mod.py
@@ -2,9 +2,11 @@
 # Автор: Кузнецов_Илья_2381
 
 from .Natural import Natural
-from div import div
-from subtract_product_from_natural import subtract_product_from_natural
+# from div import div
+# from subtract_product_from_natural import subtract_product_from_natural
 
 
 def mod(num1: Natural, num2: Natural) -> Natural:
-    pass
+    if int(num2) == 0:
+        raise ZeroDivisionError
+    return num1.subtract_product_from_natural((num1.div(num2)), int(num2))
