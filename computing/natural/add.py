edceaaa9fee827544be609e10df5351d456e35c3
dariaomnom <dariaomnom@gmail.com>
2023-10-28 22:11:44 +0300

    add (isn't done)

--- computing/natural/add.py
+++ computing/natural/add.py
@@ -2,7 +2,31 @@
 # Автор: Газукина_Дарья_2381
 
 from .Natural import Natural
-from compare import compare
+# from compare import compare
 
 def add (num1: Natural, num2: Natural) -> Natural:
-    pass
+    # if len(num1) < len(num2):
+    #     num1 += [0] * (len(num2) - len(num1))
+    # else:
+    #     num2 += [0] * (len(num1) - len(num2))
+    increased = num1.copy()
+    # added = num2.copy()
+    # if compare(num1, num2) == 2:
+    if num1.compare(num2) == 2:
+        increased.data += [0] * (len(num1) - len(num2))
+    else:
+        increased.data += [0] * (len(num2) - len(num1))
+
+    for i in range(len(increased)):
+        increased.data[i] += num2.data[i]
+        if increased.data[i] > 9:
+            increased.data[i] -= 10
+            if i + 1 < len(increased):
+                increased.data[i + 1] += 1
+            else:
+                increased.data.append(1)
+
+    while len(increased) > 1 and increased.data[-1] == 0:
+        increased.data.pop()
+
+    return increased
