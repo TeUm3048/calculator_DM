eb0865b7213d4eb00573b8058c3b961fa617b999
dariaomnom <dariaomnom@gmail.com>
2023-10-28 22:15:59 +0300

    add

--- computing/natural/add.py
+++ computing/natural/add.py
@@ -2,7 +2,7 @@
 # Автор: Газукина_Дарья_2381
 
 from .Natural import Natural
-# from compare import compare
+from compare import compare
 
 def add (num1: Natural, num2: Natural) -> Natural:
     increased = num1.copy()
