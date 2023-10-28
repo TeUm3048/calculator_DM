1f1de69451e7da14c650ccf9b2c3a789b094a59f
SaveliyKrivov <krivovsaveliy28@gmail.com>
2023-10-29 00:05:54 +0300

    mult_by_pow_of_10 fixed

--- computing/natural/multiply_by_power_of_10.py
+++ computing/natural/multiply_by_power_of_10.py
@@ -9,7 +9,7 @@ def multiply_by_power_of_10(num: Natural, k: Natural) -> Natural:
     number = num.copy()
     times = k.copy()
     number.data.reverse()
-    while times.compare(Natural(str(0))) != 0:
+    while times.is_not_zero():
         number.data.append(0)
         times = subtract(times, Natural(str(1)))
     number.data.reverse()
