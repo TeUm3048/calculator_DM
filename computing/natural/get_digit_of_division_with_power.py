f2e859b610882b03d8388ef96a5c477d45865ec2
IvanMavlikaev <zaufman@mail.ru>
2023-10-29 01:07:59 +0300

    Natural/get_digit_of_division_with_power_test

--- computing/natural/get_digit_of_division_with_power.py
+++ computing/natural/get_digit_of_division_with_power.py
@@ -5,33 +5,21 @@ from .Natural import Natural
 from .Natural import Digit
 
 def get_digit_of_division_with_power(num1: Natural, num2: Natural) -> Digit:
-    divider = 0
-    dividend = 0
-    if len(num1) > len(num2) or len(num1) == len(num2) and num1>=num2:
-        dividend = num1 
-        divider = num2
-    else:
-        dividend = num2
-        divider = num1
-    if dividend == [0]:
-        return 0
-    if divider == [0]:
-        return ZeroDivisionError
-    if dividend[:len(divider)] > divider:
-        dividend = dividend[:len(divider)]
-    else:
-        dividend = dividend[:len(divider) + 1]
+    if not num2.is_not_zero():
+        raise ZeroDivisionError
+        
+    a = num1.copy()
+    b = num2.copy()
+
+    a.data = a.data[-len(b):]
+
+    if a < b:
+        a.data = num1.data[-len(b)+1:]
+    k = Natural(str(len(num1) -  len(a)))
+
     count = Natural("0")
-    while (len(dividend) > len(divider) and (dividend[1:]>=divider or dividend[0] > 0) or
-        len(dividend) == len(divider) and dividend>=divider):
-        for i in range (1, len(divider) + 1):
-            dividend[-i] -= divider[-i]
-            if dividend[-i + 1] < 0:
-                dividend[-i + 1] += 10
-                dividend[-i] -= 1
-            if dividend[-i] < 0 and i == len(divider) and dividend[0] > 0:
-                dividend[-i] += 10
-                dividend[-i - 1] -= 1
+
+    while a >= b:
+        a -= b
         count += Natural("1")
-    return count.multiply_by_power_of_10(max(len(num1, num2)) - len(dividend))
-    # return multiply_by_power_of_10([count], max(len(num1, num2)) - len(dividend))
+    return count.multiply_by_power_of_10(k)
--- tests/computing/get_digit_of_division_with_power_test.py
+++ tests/computing/get_digit_of_division_with_power_test.py
@@ -13,6 +13,10 @@ def test_div_by_1():
     b = Natural("1")
     assert a.get_digit_of_division_with_power(b) == Natural("800000")
 
+def test_div_by_27():
+    a = Natural("814365")
+    b = Natural("27")
+    assert a.get_digit_of_division_with_power(b) == Natural("30000")
 
 def test_div_by_0():
     a = Natural("435")
