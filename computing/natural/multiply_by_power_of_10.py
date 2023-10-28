a5dd13af03c60c1cefd02f0edb092669d54d2833
SaveliyKrivov <krivovsaveliy28@gmail.com>
2023-10-28 21:23:15 +0300

    mult_by_pow_of_10 fixed

--- computing/natural/multiply_by_power_of_10.py
+++ computing/natural/multiply_by_power_of_10.py
@@ -2,11 +2,16 @@
 # Автор: Кривов_Савелий_2381
 
 from .Natural import Natural
+from .subtract import subtract
 
 
 def multiply_by_power_of_10(num: Natural, k: Natural) -> Natural:
     number = num.copy()
-    times = sum(d * (10 ** i) for i, d in enumerate(k.data))
-    for i in range(times):
-        number.data.insert(0, 0)
+    times = k.copy()
+    number.data.reverse()
+    while times.compare(Natural(str(0))) != 0:
+        number.data.append(0)
+        times = subtract(times, Natural(str(1)))
+    number.data.reverse()
+
     return number
