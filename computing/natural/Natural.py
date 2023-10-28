e333254eeca9facfb3a29dc80a3ee262822f9d56
SaveliyKrivov <krivovsaveliy28@gmail.com>
2023-10-28 19:01:20 +0300

    return fixed in Natural.py

--- computing/natural/Natural.py
+++ computing/natural/Natural.py
@@ -79,12 +79,12 @@ class Natural:
     def is_not_zero(self) -> bool:
         return not (len(self) == 1 and self.data[0] == 0)
 
-    def increment(self) -> Natural:
+    def increment(self) -> None:
         from .increment import increment
-        return increment(self)
+        increment(self)
         # self.data = Natural(str(int(self)+1)).data
         # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
-        pass
+
 
     def add(self, other: Natural) -> Natural:
         return Natural(str(int(self)+int(other)))
