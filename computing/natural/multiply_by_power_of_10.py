7da521006573cf28c749db0169c7e6fe8d04a06d
SaveliyKrivov <krivovsaveliy28@gmail.com>
2023-10-28 20:18:40 +0300

    mult_by_pow_of_10 added

--- computing/natural/multiply_by_power_of_10.py
+++ computing/natural/multiply_by_power_of_10.py
@@ -5,4 +5,8 @@ from .Natural import Natural
 
 
 def multiply_by_power_of_10(num: Natural, k: Natural) -> Natural:
-    pass
+    number = num.copy()
+    times = sum(d * (10 ** i) for i, d in enumerate(k.data))
+    for i in range(times):
+        number.data.insert(0, 0)
+    return number
