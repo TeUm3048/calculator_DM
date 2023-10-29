ee0606bbdd2bc6dac9126ebc5f5e6a3e6d59815d
Iwan Ryžij <teum3048@yandex.ru>
2023-10-29 21:38:21 +0300

    Fix multiply.py

--- computing/natural/multiply.py
+++ computing/natural/multiply.py
@@ -2,6 +2,8 @@
 # Автор: Вакуленко_Инна_2382
 
 from .Natural import Natural
+
+
 # from .multiply_by_digit import multiply_by_digit
 # from .multiply_by_power_of_10 import multiply_by_power_of_10
 # from .add import add
@@ -10,7 +12,7 @@ from .Natural import Natural
 def multiply(num1: Natural, num2: Natural) -> Natural:
     res = Natural("0")
     for i in range(len(num2)):
-        res = res.add(num1.multiply_by_digit(num2.data[i]).multiply_by_power_of_10(i))
+        k = Natural(str(i))
+        res = res.add(num1.multiply_by_digit(num2.data[i]).multiply_by_power_of_10(k))
         # res = add(multiply_by_power_of_10(multiply_by_digit(num1, num2.data[i]), Natural(str(i))), res)
     return res
-
