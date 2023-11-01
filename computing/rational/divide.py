e2d457bd86ff15f4dd7370274eeaeb5cac2c25a4
Egor Komosscky <75291783+Fel1-of@users.noreply.github.com>
2023-11-01 23:02:35 +0300

    Add Rational files (#52)

---
+++ computing/rational/divide.py
@@ -0,0 +1,13 @@
+# Модуль: DIV_ZZ_Z
+# Автор: Ваньков_Ярослав_2382
+
+from computing.rational.Rational import Rational
+from computing.integer.Integer import Integer
+
+
+def divide(self: Rational, other: Rational):
+    num1 = self.copy()
+    num2 = other.copy()
+    num2.numerator, num2.denominator = Integer.from_natural(
+        num2.denominator).multiply(Integer(str(num2.numerator.sign))), num2.numerator.number
+    return num1.multiply(num2)
