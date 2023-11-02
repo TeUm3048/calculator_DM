12d7933d221950c5a54a7d3b973ea45ee8f88c97
DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>
2023-11-03 01:47:03 +0300

    pol (#55)

--- computing/polynom/Polynom.py
+++ computing/polynom/Polynom.py
@@ -1,7 +1,14 @@
-from Rational import Rational
+from computing.natural.Natural import Natural
+from computing.integer.Integer import Integer
+from computing.rational.Rational import Rational
 
 
 class Polynom:
     data: list[Rational]
     degree: int
-    pass
+
+    def __init__(self, value: list[Rational]) -> None:
+        self.data = value
+        while len(self.data) > 1 and self.data[-1] == 0:
+            self.data.pop()
+        self.degree = len(value)
