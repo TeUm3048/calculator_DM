cc4125bb226923151cf912b324c576f2df9f7cf8
IvanMavlikaev <112758292+IvanMavlikaev@users.noreply.github.com>
2023-11-05 04:18:57 +0500

    fix- (#68)
    div

--- computing/integer/div.py
+++ computing/integer/div.py
@@ -2,8 +2,7 @@
 # Автор: Мавликаев_Иван_2381
 
 
-from .Integer import Integer
-from computing.natural.Natural import Natural
+from .Integer import Integer, Natural
 
 
 def div(num1: Integer, num2: Integer) -> Integer:
@@ -12,13 +11,13 @@ def div(num1: Integer, num2: Integer) -> Integer:
     if not num1.number.is_not_zero():
         return Integer("0")
     res = Integer(num1.number.div(num2.number))
-    if num1.sign < 0 and num2.sign > 0:
+    if num2.sign > 0 > num1.sign:
         res.sign = -1
         if num1.number != num2.number.multiply(res.number):
             res.number = res.number.add(Natural("1"))
     if num1.sign < 0 and num2.sign < 0:
         if num1.number != num2.number.multiply(res.number):
             res.number = res.number.add(Natural("1"))
-    if num1.sign > 0 and num2.sign < 0:
+    if num1.sign > 0 > num2.sign:
         res.sign = -1
     return res
