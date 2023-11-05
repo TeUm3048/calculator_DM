52b8f1da987fc115cdf3cbb861ad7a22c2b5c7d3
enshxx <kuznecovilya12@gmail.com>
2023-11-05 19:05:31 +0300

    мод сделан, но без тестов, потому что другие функции не написаны

---
+++ computing/polynom/mod.py
@@ -0,0 +1,13 @@
+# Модуль: MOD_PP_P
+# Автор: Кузнецов_Илья_2381
+
+from computing.polynom.Polynom import Polynom
+from computing.rational.Rational import Rational
+
+
+def subtract(pol1: Polynom, pol2: Polynom) -> Polynom:
+    num1 = pol1.copy()
+    num2 = pol2.copy()
+
+    return num1.subtract(num2.multiply(num1.div(num2)))
+
