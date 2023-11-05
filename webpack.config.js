01a6b65924bebd1d18499af3a4fd05debc9b562c
Ivan Ryzhikov <52576454+TeUm3048@users.noreply.github.com>
2023-11-06 02:04:25 +0300

    Move all frontend code (#80)
    -------------
    
    Co-authored-by: ksenia02romanova <125283413+ksenia02romanova@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./calculator/scr/",
+  entry: "./calculator/scr/index.tsx",
   output: {
     path: path.resolve(__dirname, "calculator/static"),
     filename: "main.js",
@@ -20,6 +20,30 @@ module.exports = {
         use: {
           loader: "ts-loader",
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10000,
+          },
+        },
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ["@svgr/webpack"],
       },
     ],
   },
