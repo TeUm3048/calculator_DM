95214de1a083ba23be6748073c8e3df9cdf38676
Ilya Bogatov <111486509+Cuga77@users.noreply.github.com>
2023-11-05 16:25:55 +0300

    Update get_leading_coefficient.py

--- computing/polynom/get_leading_coefficient.py
+++ computing/polynom/get_leading_coefficient.py
@@ -4,7 +4,4 @@ from .Polynom import Polynom
 
 
 def get_leading_coefficient(self: Polynom):
-    if len(self.data) > 0:
-        return self.data[-1]
-    else:
-        return None
+    return self.data[-1]
