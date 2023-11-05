77e449f54db51840aa865384f1ca394ba647dae7
dariaomnom <dariaomnom@gmail.com>
2023-11-05 21:11:57 +0300

    derive with const

--- computing/polynom/derive.py
+++ computing/polynom/derive.py
@@ -6,15 +6,14 @@ from computing.rational.Rational import Rational
 
 def derive (pol: Polynom) -> Polynom:
     derivative = pol.copy()
-    for i in range(len(derivative.data)-1):
-        derivative.data[i] = derivative.data[i+1]
-        degree = Rational(str(i + 1) + '/1')
-        # degree = Rational([i + 1, 1])
-        derivative.data[i] = derivative.data[i].multiply(degree)
-    derivative.data.pop(-1)
-    return derivative
+    if len(derivative.data) > 1:
+        for i in range(len(derivative.data)-1):
+            derivative.data[i] = derivative.data[i+1]
+            degree = Rational(str(i + 1) + '/1')
+            # degree = Rational([i + 1, 1])
+            derivative.data[i] = derivative.data[i].multiply(degree)
+        derivative.data.pop(-1)
+    else:
+        derivative.data[0] = Rational('0')
 
-# if __name__ == '__main__':
-#     a = Polynom([Rational("5"), Rational("4"), Rational("0"), Rational("-6"), Rational("5")])
-#     b = a.derive()
-#     curr_res = Polynom([Rational("4"), Rational("-18"), Rational("20")])
+    return derivative
