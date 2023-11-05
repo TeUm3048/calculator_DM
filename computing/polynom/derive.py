06453c127b130220151bc6143466e0d3245a649a
dariaomnom <dariaomnom@gmail.com>
2023-11-05 21:48:14 +0300

    derive beautiful code

--- computing/polynom/derive.py
+++ computing/polynom/derive.py
@@ -4,15 +4,16 @@
 from .Polynom import Polynom
 from computing.rational.Rational import Rational
 
-def derive (pol: Polynom) -> Polynom:
+def derive(pol: Polynom) -> Polynom:
     derivative = pol.copy()
-    if len(derivative.data) > 1:
-        for i in range(len(derivative.data)-1):
-            derivative.data[i] = derivative.data[i+1]
-            degree = Rational(str(i + 1))
-            derivative.data[i] = derivative.data[i].multiply(degree)
-        derivative.data.pop(-1)
-    else:
+    if len(derivative.data) <= 1:
         derivative.data[0] = Rational('0')
+        return derivative
+
+    for i in range(len(derivative.data) - 1):
+        derivative.data[i] = derivative.data[i + 1]
+        degree = Rational(str(i + 1))
+        derivative.data[i] = derivative.data[i].multiply(degree)
+    derivative.data.pop(-1)
 
     return derivative
