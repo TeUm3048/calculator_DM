0846c5588033f0e6f52e4c1493275f796fcf7a6b
IvanMavlikaev <zaufman@mail.ru>
2023-11-06 14:47:50 +0300

    fix_gcd

--- computing/polynom/gcd.py
+++ computing/polynom/gcd.py
@@ -8,12 +8,15 @@ from .Polynom import Polynom
 def gcd(self: Polynom, other: Polynom)-> Polynom:
     pol1 = self.copy()
     pol2 = other.copy()
-
-    while pol1.get_degree() > 0 and pol2.get_degree() > 0:
-        if pol1.get_degree() > pol2.get_degree():
+    deg1 = pol1.get_degree()
+    deg2 = pol2.get_degree()
+    while deg1 > 0 and deg2 > 0:
+        if deg1 > deg2:
             pol1 = pol1.mod(pol2)
+            deg1 = pol1.get_degree()
         else:
             pol2 = pol2.mod(pol1)
+            deg2 = deg2.get_degree()
     if pol1.get_degree() == 0:
         return pol2
     return pol1
