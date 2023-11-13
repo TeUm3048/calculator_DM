208c743d0485fcc9e7bc92770ef9a94f05c39bd0
monsier-chess <monsier.sadness@gmail.com>
2023-11-13 15:09:05 +0300

    fix_gcd

--- computing/polynom/gcd.py
+++ computing/polynom/gcd.py
@@ -10,13 +10,13 @@ def gcd(self: Polynom, other: Polynom)-> Polynom:
     pol2 = other.copy()
     deg1 = pol1.get_degree()
     deg2 = pol2.get_degree()
-    while deg1 > 0 and deg2 > 0:
+    while not pol1.is_null() and not pol2.is_null():
         if deg1 > deg2:
             pol1 = pol1.mod(pol2)
             deg1 = pol1.get_degree()
         else:
             pol2 = pol2.mod(pol1)
             deg2 = pol2.get_degree()
-    if deg1 == 0:
+    if pol1.is_null():
         return pol2
     return pol1
