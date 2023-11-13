67d955df38d4674efc453e43ba39130c1049433d
IvanMavlikaev <112758292+IvanMavlikaev@users.noreply.github.com>
2023-11-13 18:55:22 +0300

    Fix gcd (#94)
    * fix_gcd
    
    * fix_gcd
    
    ---------
    
    Co-authored-by: monsier-chess <monsier.sadness@gmail.com>

--- computing/polynom/gcd.py
+++ computing/polynom/gcd.py
@@ -3,6 +3,7 @@
 
 #НОД многочленов
 from .Polynom import Polynom
+from computing.rational.Rational import Rational
 
 
 def gcd(self: Polynom, other: Polynom)-> Polynom:
@@ -10,13 +11,17 @@ def gcd(self: Polynom, other: Polynom)-> Polynom:
     pol2 = other.copy()
     deg1 = pol1.get_degree()
     deg2 = pol2.get_degree()
-    while deg1 > 0 and deg2 > 0:
-        if deg1 > deg2:
+    while not pol1.is_null() and not pol2.is_null():
+        if deg1 > deg2 or deg1 == deg2 and pol1.data[-1] > pol2.data[-1]:
             pol1 = pol1.mod(pol2)
             deg1 = pol1.get_degree()
         else:
             pol2 = pol2.mod(pol1)
             deg2 = pol2.get_degree()
-    if deg1 == 0:
+    if pol1.is_null():
+        if deg2 == 0 and pol2.data[0] < Rational("0"):
+            pol2 = pol2.multiply_by_scalar(Rational("-1"))
         return pol2
+    if deg1 == 0 and pol1.data[0] < Rational("0"):
+        pol1 = pol1.multiply_by_scalar(Rational("-1"))
     return pol1
