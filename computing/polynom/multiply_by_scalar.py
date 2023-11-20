a9e161d946ab8c4560b5ebd0485b22d38caba457
DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>
2023-11-20 23:41:54 +0300

    Update multiply_by_scalar.py

--- computing/polynom/multiply_by_scalar.py
+++ computing/polynom/multiply_by_scalar.py
@@ -4,9 +4,14 @@
 from computing.polynom.Polynom import Polynom
 from computing.rational.Rational import Rational
 
-
+# Умножение на скаляр
 def multiply_by_scalar(pol1: Polynom, scalar: Rational) -> Polynom:
+    
+    # Копирование исходных чисел для дальнейшей работы
     pol = pol1.copy()
+    
+    # Цикл, в котором каждый итерируемый коэффициент умножается на скаляр
     for i in range(len(pol)):
         pol.data[i] = pol.data[i].multiply(scalar)
+
     return pol
