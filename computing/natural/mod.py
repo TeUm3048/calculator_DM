80ce4e01820d93489171deb5ae3d3f1f6955519b
monsier-chess <68637934+monsier-chess@users.noreply.github.com>
2023-11-20 19:43:08 +0300

    Прокомментирован модуль Natural (#109)
    Co-authored-by: DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>

--- computing/natural/mod.py
+++ computing/natural/mod.py
@@ -2,11 +2,11 @@
 # Автор: Кузнецов_Илья_2381
 
 from .Natural import Natural
-# from div import div
-# from subtract_product_from_natural import subtract_product_from_natural
 
-
+# Остаток от деления
 def mod(num1: Natural, num2: Natural) -> Natural:
+    # MOD не определён для второго нулевого значения
     if not num2.is_not_zero():
         raise ZeroDivisionError
+    # MOD можно выразить через DIV из формулы  a = mod(a, b) + div(a, b)
     return num1.subtract(num1.div(num2).multiply(num2))
