81bf7f611aae7d6f786ae1fcb95b110e0dfd7664
Egor Komosscky <75291783+Fel1-of@users.noreply.github.com>
2023-11-20 18:25:43 +0300

    Add comments in Ёгор's moduls (#110)

--- computing/integer/add.py
+++ computing/integer/add.py
@@ -3,21 +3,28 @@
 
 from .Integer import Integer
 
-
+# Сложение двух чисел
 def add(self: Integer, other: Integer) -> Integer:
     num1 = self.copy()
     num2 = other.copy()
+    # Сравнение знаков у чисел
     if num1.determinate_sign() == num2.determinate_sign():
+        # Если знаки одинаковые, то складываем абсолютные значения и возвращаем сумму с изначальным знаком
         num1.number = num1.number.add(num2.number)
         return num1
+    # Если знаки не равны, то сравниваем абсолютные значения
     match num1.number.compare(num2.number):
         case 2:
+            # Если первое абсолютное значение первого числа больше, чем абс. значение второго,
+            # то вычитаем из первого второе и возвращаем со знаком первого числа
             num1.number = num1.number.subtract(num2.number)
             return num1
         case 1:
+            # Если второе больше первого, то вычитаем из второго первое и возвращаем со знаком второго числа
             num2.number = num2.number.subtract(num1.number)
             return num2
         case 0:
+            # Если абсолютные значения равны, то возвращаем 0
             return Integer('0')
 
 
--- computing/polynom/factor_polynomial_coefficients.py
+++ computing/polynom/factor_polynomial_coefficients.py
@@ -6,13 +6,17 @@ from computing.natural.Natural import Natural
 from computing.rational.Rational import Rational
 from computing.integer.Integer import Integer
 
-
+# Факторизация коэфициентов многочлена
 def factor_polynomial_coefficients(self: Polynom) -> Polynom:
+    # Для подсчёта НОК и НОД записываем знаменатель и числитель первого коэффициента
     nk = self.data[0].denominator
+    # Если первый коэф. равен 0, то записываем 1, чтобы функция подсчёта НОД работала корректно
     nd = self.data[0].numerator.absolute() if self.data[0].numerator.absolute() != Natural('0') else Natural('1')
+    # Проходимся в цикле по всем коэф-ам многочлена и рассчитываем НОК и НОД
     for i in range(len(self.data)):
         nk = Natural.lcm(nk, self.data[i].denominator)
         nd = Natural.gcd(nd, self.data[i].numerator.absolute()) if \
             self.data[i].numerator.absolute() != Natural('0') else nd
+    # Домножаем все коэф-ты на НОК и делим на НОД
     ans = [Rational.simplify(Rational.multiply(i, Rational([Integer.from_natural(nk), nd]))) for i in self.data]
     return Polynom(ans)
