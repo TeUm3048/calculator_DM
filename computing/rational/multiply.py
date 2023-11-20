e9e28f314f085cb55f373a2ac646f7ecff20f9d1
Inna Vakulenko <invackk@gmail.com>
2023-11-20 21:14:58 +0300

    Comments for Rational: Done (#113)
    Co-authored-by: DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>

--- computing/rational/multiply.py
+++ computing/rational/multiply.py
@@ -4,8 +4,13 @@
 from computing.rational.Rational import Rational
 
 
+# Умножение двух рациональных чисел
 def multiply(self: Rational, other: Rational) -> Rational:
+
+    # Инициализация двух чисел на основе переданных в функцию
     num1 = self.copy()
     num2 = other.copy()
+
+    # Умножение, упрощение и возврат нового числа
     return Rational([num1.numerator.multiply(num2.numerator),
                      num1.denominator.multiply(num2.denominator)]).simplify()
