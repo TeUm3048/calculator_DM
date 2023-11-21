5b7c10650204b0a2a2bbf77d062b65197841cdc8
DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>
2023-11-21 20:21:28 +0300

    Update serializers.py

--- calculator/calculator/serializers.py
+++ calculator/calculator/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 
 
 class NaturalSerializer(serializers.Serializer):
-    num = serializers.CharField(max_length=200)
+    num = serializers.CharField(max_length=100000)
 
 
 class IntegerSerializer(serializers.Serializer):
-    num = serializers.CharField(max_length=200)
+    num = serializers.CharField(max_length=100000)
 
 
 class RationalSerializer(serializers.Serializer):
-    num = serializers.CharField(max_length=200)
+    num = serializers.CharField(max_length=100000)
 
 
 class PolynomSerializer(serializers.Serializer):
