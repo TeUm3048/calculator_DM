c21084fe1745c0011837e197c82a33cb77e24736
Iwan Ryžij <teum3048@yandex.ru>
2023-11-21 16:55:47 +0300

    Add docs

---
+++ calculator/scr/Pages/DocsPage/DocsPage.tsx
@@ -0,0 +1,12 @@
+import Docs from "../../docs/Docs.md";
+import styles from "./DocsPage.module.css";
+
+const DocsPage = () => {
+  return (
+    <div className={styles.wrap}>
+      <Docs></Docs>
+    </div>
+  );
+};
+
+export default DocsPage;
