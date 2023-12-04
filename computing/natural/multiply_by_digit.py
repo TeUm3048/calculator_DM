8e8364e3f0a57a9b09ad743021af43b2b1ca2230
Cuga77 <bogatov2021@mail.ru>
2023-12-04 11:40:35 +0300

    Fix_multiply_by_digit

--- computing/natural/multiply_by_digit.py
+++ computing/natural/multiply_by_digit.py
@@ -6,7 +6,6 @@ from .Natural import Digit
 
 # Умножение на цифру
 def multiply_by_digit(num: Natural, digit: Digit) -> Natural:
-
     # Обработка тривиального случая digit = 0
     if digit == 0:
         return Natural('0')
@@ -14,22 +13,24 @@ def multiply_by_digit(num: Natural, digit: Digit) -> Natural:
     # carry - "В уме", то что переносится на следующие разряды
     # result - Массив цифр итогово ответа
     carry, result = 0, []
-    
+
     # Последовательная обработка разрядов
     for d in num.data:
-        
         # total - результат умножения текущей цифры, с учётом переноса
         total = d * digit + carry
-        
+
         # Запись последней цифры произведения в ответ
         result.append(total % 10)
-        
+
         # Пересчёт переносимой части произведения
         carry = total // 10
-    
+
     # Добавление разряда при необходимости
     if carry > 0:
         result.append(carry)
 
-    # Создание и возврат нового числа на основе массива цифр
-    return Natural(''.join(map(str, result[::-1])))
+    # Обновление массива цифр в переданном числе
+    num.data = result[::-1]
+
+    # Возврат переданного числа
+    return num
